09c1a2b0176b14a6f14358ea7f1c44563db41259
CAEUser <e37f44db2937b2e33c487273d264a29359dc4138>
2020-07-17 10:44:48 +0000

    Generated microservice version 0.1.0

---
+++ db/MetadataTest3_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  cae-schema
+-- Automatically generated sql script for the service MetadataTest3, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE cae-schema.tab (
+  col INTEGER ,
+CONSTRAINT col_PK PRIMARY KEY (col)
+ 
+);
+
+
+
